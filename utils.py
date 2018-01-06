d6b6455b395ba84e6ea3d00ce64e2d199d5ceecc
Vincent Rialland <vincent.rialland@gmail.com>
2018-01-06 14:23:16 +0100

    Add iterations argument

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import utime
 
 
-def timeit(func, *args, **kwargs):
+def timeit(func, iterations=1, *args, **kwargs):
     start = utime.ticks_ms()
-    func(*args, **kwargs)
+    for i in range(iterations):
+        func(*args, **kwargs)
     stop = utime.ticks_ms()
-    return utime.ticks_diff(stop, start)
+    diff = utime.ticks_diff(stop, start)
+    print('{0} iteration(s) took {1:.3}ms'.format(iterations, diff))
